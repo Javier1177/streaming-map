70d125328ca3a166a42b21685cffc8016d7ccad0
Javi Lopez <javierlopezblasco@gmail.com>
2021-05-01 11:50:00 +0200

    Creating project structure

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MapContainer = () => <div className="mapContainer"></div>;
+
+export default MapContainer;
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Map from '../../components/Map/Map';
+
+const MainPage = () => (
+  <div>
+    <Map />
+  </div>
+);
+export default MainPage;

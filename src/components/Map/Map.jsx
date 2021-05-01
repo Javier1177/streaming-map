af93961abbbdf400c74277c5bbd8e6084b897539
Javi Lopez <javierlopezblasco@gmail.com>
2021-05-01 12:59:09 +0200

    Finish adding google maps with default values

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const MapContainer = () => <div className="mapContainer"></div>;
+const LoadingContainer = (props) => <div>Fancy loading container!</div>;
 
-export default MapContainer;
+const MapContainer = ({ google }) => (
+  <div className="mapContainer">
+    <Map
+      google={google}
+      zoom={8}
+      initialCenter={{ lat: 47.444, lng: -122.176 }}
+    >
+      <Marker position={{ lat: 48.0, lng: -122.0 }} />
+    </Map>
+  </div>
+);
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  LoadingContainer: LoadingContainer,
+})(MapContainer);
